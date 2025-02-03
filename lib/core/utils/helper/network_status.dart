67c062d4819debb4173f66a1255b7d20bd42ece2
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2025-02-03 02:39:30 +0200

    Add network connectivity checks and refactor related services

---
+++ lib/core/utils/helper/network_status.dart
@@ -0,0 +1,24 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+class NetworkHelper {
+  static final Connectivity _connectivity = Connectivity();
+
+  static Future<bool> isConnected() async {
+    final List<ConnectivityResult> results =
+        await _connectivity.checkConnectivity();
+    return _isConnected(results);
+  }
+
+  static bool _isConnected(List<ConnectivityResult> results) {
+    return results.any((result) =>
+        result == ConnectivityResult.mobile ||
+        result == ConnectivityResult.wifi ||
+        result == ConnectivityResult.ethernet ||
+        result == ConnectivityResult.vpn);
+  }
+
+  static Stream<bool> get connectionStatusStream {
+    return _connectivity.onConnectivityChanged
+        .asyncMap((results) => _isConnected(results));
+  }
+}
