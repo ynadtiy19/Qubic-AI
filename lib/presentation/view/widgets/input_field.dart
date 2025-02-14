b957a578cc65a9f2c427d0cbd0b2f7fa939511b4
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2025-02-14 12:04:24 +0200

    Add InputFieldViewModel for managing input field state and image selection

--- lib/presentation/view/widgets/input_field.dart
+++ lib/presentation/view/widgets/input_field.dart
@@ -1,22 +1,35 @@
+import 'dart:io';
+
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:loading_indicator/loading_indicator.dart';
+import 'package:popup_menu/popup_menu.dart';
+import 'package:qubic_ai/core/di/locator.dart';
+import 'package:qubic_ai/core/service/text_recognition.dart';
 import 'package:qubic_ai/core/utils/extension/extension.dart';
 
+import '../../../core/service/image_packer.dart';
 import '../../../core/utils/constants/colors.dart';
+import '../../../core/utils/helper/custom_toast.dart';
 import '../../../core/utils/helper/regexp_methods.dart';
-import '../../viewmodel/chat/chat_bloc.dart';
+import '../../bloc/chat/chat_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../bloc/input/input_field_bloc.dart';
+import '../../viewmodel/input_field_viewmodel.dart';
 
 class BuildInputField extends StatefulWidget {
-  const BuildInputField(
-      {super.key,
-      required this.generativeAIBloc,
-      required this.isLoading,
-      required this.chatId,
-      required this.isChatHistory});
+  const BuildInputField({
+    super.key,
+    required this.chatBloc,
+    required this.isLoading,
+    required this.chatId,
+    required this.isChatHistory,
+  });
 
-  final ChatAIBloc generativeAIBloc;
+  final ChatBloc chatBloc;
   final bool isLoading;
   final int? chatId;
   final bool isChatHistory;
@@ -26,128 +39,207 @@ class BuildInputField extends StatefulWidget {
 }
 
 class _BuildInputFieldState extends State<BuildInputField> {
-  late TextEditingController _textInputFieldController;
+  late final InputFieldBloc _inputFieldBloc;
+  late final InputFieldViewModel _viewModel;
+  late GlobalKey _popupMenuKey;
 
   @override
   void initState() {
     super.initState();
-    _textInputFieldController = TextEditingController();
+    _inputFieldBloc = InputFieldBloc();
+    _viewModel = InputFieldViewModel(
+      inputFieldBloc: _inputFieldBloc,
+      generativeAIBloc: widget.chatBloc,
+      imagePickerService: getIt<ImagePickerService>(),
+      textRecognitionService: getIt<TextRecognitionService>(),
+    );
+    _popupMenuKey = GlobalKey();
   }
 
-  void _onChanged(String text) {
-    if (_isFieldEmpty) {
-      _isFieldEmpty = false;
-      setState(() {});
-    } else if (text.trim().length == 1 || text.trim().isEmpty) {
-      if (text.trim().isEmpty) {
-        _isFieldEmpty = true;
-      }
-      setState(() {});
-    }
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    _inputFieldBloc.close();
+    super.dispose();
   }
 
-  void _sendMessage() {
-    if (_textInputFieldController.text.trim().isNotEmpty) {
-      widget.generativeAIBloc.add(
-        StreamDataEvent(
-          prompt: _textInputFieldController.text.trim(),
-          isUser: true,
-          chatId: widget.chatId ?? 0,
-        ),
-      );
-      _textInputFieldController.clear();
-    }
-    _isFieldEmpty = true;
-    setState(() {});
+  void showMessageBox() {
+    final menuItems = [
+      _buildMenuItem("Camera", Icons.camera_alt_rounded),
+      _buildMenuItem("Gallery", Icons.photo_library_rounded),
+      if (!widget.isChatHistory) _buildMenuItem("Chat", Icons.add),
+    ];
+
+    PopupMenu(
+      context: context,
+      config: const MenuConfig(
+        itemWidth: 110,
+        arrowHeight: 12,
+        backgroundColor: Color(0xFF1F1F1F),
+        lineColor: ColorManager.white,
+        highlightColor: ColorManager.purple,
+        type: MenuType.list,
+      ),
+      items: menuItems,
+      onClickMenu: (item) async {
+        bool res = true;
+        if (item.menuTitle == 'Camera') {
+          res = await _viewModel.pickImage(ImageSource.camera);
+        } else if (item.menuTitle == 'Gallery') {
+          res = await _viewModel.pickImage(ImageSource.gallery);
+        } else if (item.menuTitle == 'Chat') {
+          widget.chatBloc.add(const CreateNewChatSessionEvent());
+        }
+        if (!res) {
+          showCustomToast(context,
+              message: 'No text recognized in the image',
+              color: ColorManager.error);
+        }
+      },
+    ).show(widgetKey: _popupMenuKey);
   }
 
-  bool _isFieldEmpty = true;
+  MenuItem _buildMenuItem(String title, IconData icon) {
+    return MenuItem(
+      title: title,
+      textStyle: context.textTheme.bodySmall
+          ?.copyWith(fontWeight: FontWeight.w500, fontSize: 11.spMin),
+      image: Icon(icon, color: ColorManager.white, size: 22),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return FadeInUp(
-      curve: Curves.easeInOut,
-      duration: const Duration(milliseconds: 300),
-      child: Container(
-        margin: EdgeInsets.only(
-            right: 9.w,
-            left: 9.w,
-            bottom: MediaQuery.of(context).viewInsets.bottom + 8.h),
-        decoration: BoxDecoration(
-          color: ColorManager.grey.withValues(alpha: 0.12),
-          borderRadius: BorderRadius.circular(30),
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: 5.w, top: 5.w, bottom: 5.w),
-              child: !widget.isChatHistory
-                  ? IconButton(
-                      onPressed: () => widget.generativeAIBloc
-                          .add(CreateNewChatSessionEvent()),
-                      icon: Icon(
-                        Icons.add,
-                        color: ColorManager.white,
-                        size: 25.w,
-                      ),
-                    )
-                  : null,
-            ),
-            Expanded(
-              child: TextField(
-                minLines: 1,
-                maxLines: 5,
-                onChanged: _onChanged,
-                style: context.textTheme.bodyMedium,
-                controller: _textInputFieldController,
-                textDirection: RegExpManager.getFieldDirection(
-                    _textInputFieldController.text),
-                onSubmitted: (_) => !widget.isLoading ? _sendMessage() : null,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.only(
-                      left: !widget.isChatHistory ? 0 : 15,
-                      right: 10,
-                      top: 13.h,
-                      bottom: 13.h),
-                  hintText: 'Message Qubic AI',
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(5.w),
-              child: IconButton(
-                style: IconButton.styleFrom(
-                  enableFeedback:
-                      _textInputFieldController.text.trim().isNotEmpty,
-                  overlayColor: _textInputFieldController.text.trim().isEmpty
-                      ? Colors.transparent
-                      : null,
-                  backgroundColor: widget.isLoading
-                      ? ColorManager.white
-                      : _textInputFieldController.text.trim().isEmpty
-                          ? ColorManager.grey
-                          : ColorManager.white,
-                ),
-                onPressed: () => !widget.isLoading ? _sendMessage() : null,
-                icon: widget.isLoading
-                    ? SizedBox(
-                        height: 25.w,
-                        width: 25.w,
-                        child: const LoadingIndicator(
-                          indicatorType: Indicator.lineSpinFadeLoader,
+    return BlocBuilder<InputFieldBloc, InputFieldState>(
+      bloc: _inputFieldBloc,
+      builder: (context, state) {
+        return FadeInUp(
+          curve: Curves.easeInOut,
+          duration: const Duration(milliseconds: 300),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              if (state.selectedImage != null)
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Image.file(
+                          File(state.selectedImage!),
+                          fit: BoxFit.cover,
+                          width: 150,
+                          height: 100,
                         ),
-                      )
-                    : Icon(
-                        Icons.arrow_upward_rounded,
-                        color: ColorManager.dark,
-                        size: 25.w,
                       ),
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: IconButton(
+                          style: IconButton.styleFrom(
+                            backgroundColor:
+                                ColorManager.black.withValues(alpha: 0.4),
+                          ),
+                          icon: const Icon(
+                            Icons.close,
+                            color: ColorManager.white,
+                            size: 20,
+                          ),
+                          onPressed: () => _inputFieldBloc.add(UpdateImageEvent(
+                              selectedImage: null, recognizedText: null)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              Container(
+                margin: EdgeInsets.only(
+                    right: 9, left: 9, bottom: context.viewInsetsBottom + 8),
+                decoration: BoxDecoration(
+                  color: ColorManager.grey.withValues(alpha: 0.12),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(left: 5, top: 5, bottom: 5),
+                      child: IconButton(
+                        key: _popupMenuKey,
+                        onPressed: showMessageBox,
+                        icon: const Icon(
+                          Icons.add,
+                          color: ColorManager.white,
+                          size: 25,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: TextField(
+                        minLines: 1,
+                        maxLines: 5,
+                        style: context.textTheme.bodyMedium
+                            ?.copyWith(fontSize: 15.spMin),
+                        controller: _viewModel.textController,
+                        textDirection: RegExpManager.getFieldDirection(
+                            _viewModel.textController.text),
+                        decoration: const InputDecoration(
+                          contentPadding: EdgeInsets.only(
+                              left: 10, right: 10, top: 18, bottom: 18),
+                          hintText: 'Message Qubic AI',
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: IconButton(
+                        style: IconButton.styleFrom(
+                          enableFeedback: _viewModel.textController.text
+                                  .trim()
+                                  .isNotEmpty ||
+                              state.selectedImage != null,
+                          overlayColor:
+                              _viewModel.textController.text.trim().isEmpty &&
+                                      state.selectedImage == null
+                                  ? Colors.transparent
+                                  : null,
+                          backgroundColor: widget.isLoading
+                              ? ColorManager.white
+                              : _viewModel.textController.text.trim().isEmpty &&
+                                      state.selectedImage == null
+                                  ? ColorManager.grey
+                                  : ColorManager.white,
+                        ),
+                        onPressed: () => !widget.isLoading
+                            ? _viewModel.sendMessage(
+                                chatId: widget.chatId,
+                                isChatHistory: widget.isChatHistory,
+                              )
+                            : null,
+                        icon: widget.isLoading
+                            ? const SizedBox(
+                                height: 25,
+                                width: 25,
+                                child: LoadingIndicator(
+                                  indicatorType: Indicator.lineSpinFadeLoader,
+                                ),
+                              )
+                            : Icon(
+                                Icons.arrow_upward_rounded,
+                                color: ColorManager.dark,
+                                size: 25,
+                              ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
-      ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
---
+++ lib/presentation/viewmodel/input_field_viewmodel.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+import '../../core/service/image_packer.dart';
+import '../../core/service/text_recognition.dart';
+import '../bloc/chat/chat_bloc.dart';
+import '../bloc/input/input_field_bloc.dart';
+
+class InputFieldViewModel {
+  final TextEditingController _textController = TextEditingController();
+  final InputFieldBloc inputFieldBloc;
+  final ChatBloc generativeAIBloc;
+  final ImagePickerService imagePickerService;
+  final TextRecognitionService textRecognitionService;
+
+  InputFieldViewModel({
+    required this.inputFieldBloc,
+    required this.generativeAIBloc,
+    required this.imagePickerService,
+    required this.textRecognitionService,
+  }) {
+    _textController.addListener(_handleTextChanged);
+  }
+
+  TextEditingController get textController => _textController;
+  void _handleTextChanged() {
+    final text = _textController.text.trim();
+    final isFieldEmpty = text.isEmpty;
+    inputFieldBloc.add(UpdateFieldEvent(isFieldEmpty));
+  }
+
+  // bool _isFieldEmpty = true;
+
+  // void _handleTextChanged() {
+  //   final text = _textController.text.trim();
+  //   if (_isFieldEmpty) {
+  //     _isFieldEmpty = false;
+  //     inputFieldBloc.add(UpdateFieldEvent(_isFieldEmpty));
+  //   } else if (text.length == 1 || text.isEmpty) {
+  //     if (text.isEmpty) {
+  //       _isFieldEmpty = true;
+  //     }
+  //     inputFieldBloc.add(UpdateFieldEvent(_isFieldEmpty));
+  //   }
+  // }
+  Future<bool> pickImage(ImageSource source) async {
+    try {
+      final image = await imagePickerService.pickImage(source);
+      if (image != null) {
+        final recognizedText = await textRecognitionService.extractText(image);
+        if (recognizedText.isEmpty) {
+          throw Exception("Image selected but no text recognized.");
+        }
+        inputFieldBloc.add(UpdateImageEvent(
+          selectedImage: image.path,
+          recognizedText: recognizedText,
+        ));
+        return true;
+      }
+      return false;
+    } catch (_) {
+      return false;
+    }
+  }
+
+  void sendMessage({required int? chatId, required bool isChatHistory}) {
+    final text = _textController.text.trim();
+    final currentState = inputFieldBloc.state;
+
+    if (text.isNotEmpty || currentState.selectedImage != null) {
+      generativeAIBloc.add(StreamDataEvent(
+        prompt: text,
+        isUser: true,
+        chatId: chatId ?? 0,
+        image: (currentState.recognizedText?.isNotEmpty ?? false)
+            ? currentState.selectedImage
+            : null,
+        recognizedText: currentState.recognizedText,
+      ));
+
+      _textController.clear();
+      inputFieldBloc.add(ResetInputFieldEvent());
+    }
+  }
+
+  void dispose() {
+    _textController.dispose();
+  }
+}
